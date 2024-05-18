8ebf84badd1e8d0f5d54b491b8ca4f583d7fa9ee
Milan Vit <milanvit@milanvit.net>
2024-05-19 00:05:16 +0900

    Prepare for Minetti OS
    Also drop support for other hardware than Raspberry Pi Zero

--- mix.exs
+++ mix.exs
@@ -3,19 +3,7 @@ defmodule MinettiFirmware.MixProject do
 
   @app :minetti_firmware
   @version "0.1.0"
-  @all_targets [
-    :rpi,
-    :rpi0,
-    :rpi2,
-    :rpi3,
-    :rpi3a,
-    :rpi4,
-    :bbb,
-    :osd32mp1,
-    :x86_64,
-    :grisp2,
-    :mangopi_mq_pro
-  ]
+  @all_targets [:rpi0, :minetti_os]
 
   def project do
     [
@@ -60,17 +48,9 @@ defmodule MinettiFirmware.MixProject do
       # bumps to Nerves systems. Since these include Linux kernel and Erlang
       # version updates, please review their release notes in case
       # changes to your application are needed.
-      {:nerves_system_rpi, "~> 1.24", runtime: false, targets: :rpi},
       {:nerves_system_rpi0, "~> 1.24", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.24", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.24", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.24", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.24", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.19", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.15", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.24", runtime: false, targets: :x86_64},
-      {:nerves_system_grisp2, "~> 0.8", runtime: false, targets: :grisp2},
-      {:nerves_system_mangopi_mq_pro, "~> 0.6", runtime: false, targets: :mangopi_mq_pro}
+      {:minetti_os,
+       path: "../minetti_os", runtime: false, targets: :minetti_os, nerves: [compile: true]}
     ]
   end
 
